8365f0a9aee49d13b9c117564e507aa3ac15489b
Emil Libera <emil.libera@mobica.com>
2021-10-15 10:31:10 +0200

    add reanimated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
 };
